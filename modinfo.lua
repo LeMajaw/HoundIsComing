fe5ab24f896a50eafbf60d1733b1498090177630
LeMajaw <54512834+LeMajaw@users.noreply.github.com>
2019-08-25 18:02:17 -0300

    Upload dos arquivos
    Upload dos arquivos que se encontravam no host

---
+++ modinfo.lua
@@ -0,0 +1,64 @@
+name = "Hound is Coming"
+version = "1.2.0"
+author = "Majaw"
+description = "Tips to the hound attack"
+forumthread = ""
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+dst_compatible = true
+
+server_filter_tags = {}
+
+api_version = 6
+api_version_dst = 10
+
+client_only_mod = true
+all_clients_require_mod = false
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+configuration_options =
+{
+	{
+		name = "enable_houndattack",
+		label = "Enable Hound Attack",
+		options =	{
+						{description = "True", data = true},
+						{description = "False", data = false}
+					},
+
+		default = true,
+	
+	},
+	{
+		name = "format",
+		label = "String format",
+		options =	{
+						{description = "Complex", data = "complex"},
+						{description = "Simplex", data = "simplex"}
+					},
+
+		default = "complex",
+	
+	},
+	{
+		name = "days",
+		label = "Days in advance",
+		options =	{
+						{description = "7 Days", data = 7},
+						{description = "6 Days", data = 6},
+						{description = "5 Day", data = 5},
+						{description = "4 Days", data = 4},
+						{description = "3 Days", data = 3},
+						{description = "2 Days", data = 2},
+						{description = "1 Day", data = 1},
+						{description = "Attack Day", data = 0}
+					},
+
+		default = 3,
+	
+	}
+}
---
+++ modmain.lua
@@ -0,0 +1,70 @@
+local Widget = GLOBAL.require('widgets/widget')
+
+local ENABLE_HOUND_ATTACK = GetModConfigData("enable_houndattack")
+local STRING_FORMAT       = GetModConfigData("format")
+local DAYS_IN_ADVANCE     = GetModConfigData("days")
+
+-- A full Day lasts for 8 real-time minutes
+local secADay = 8 * 60
+local function second2Day(val) 
+	return math.floor(val / secADay)
+end
+local function attackString(timeToAttack)
+
+	if STRING_FORMAT == "complex" then
+
+		if timeToAttack == 0 then
+			return 'Hound Attack: Today!'
+		elseif timeToAttack == 1 then
+			return 'Hound Attack: 1 day left'
+		end
+
+		return 'Hound Attack: ' .. timeToAttack  .. ' days left'
+
+	elseif STRING_FORMAT == "simplex" then
+
+		if timeToAttack == 0 then
+			return 'Today!'
+		elseif timeToAttack == 1 then
+			return '1 Day'
+		end
+
+		return timeToAttack  .. ' Days'
+
+	end
+end
+
+local function HoundAttack(inst)
+
+	inst:ListenForEvent("cycleschanged",
+
+		function(inst)
+
+			if GLOBAL.TheWorld:HasTag("cave") then
+				return
+			end
+
+			if not GLOBAL.TheWorld.components.hounded then
+				return
+			end
+			
+			local _timeToAttack = GLOBAL.TheWorld.components.hounded:GetTimeToAttack()
+			local timeToAttack  = second2Day(_timeToAttack)
+
+			if timeToAttack <= DAYS_IN_ADVANCE and GLOBAL.TheWorld.state.cycles ~= 0 then
+				GLOBAL.TheNet:Announce(attackString(timeToAttack))
+			end
+			
+			print("Hound attack: " .. _timeToAttack)
+		end,
+
+		GLOBAL.TheWorld)
+
+end
+
+
+if ENABLE_HOUND_ATTACK then
+	
+	AddPrefabPostInit("world", HoundAttack)
+	
+end
